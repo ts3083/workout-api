5d7374ba27f0d2b5d3dc27aeea3f65b7238fd93c
ts3083 <ts04031@gmail.com>
2023-04-23 23:50:10 +0900

    Initial commit

---
+++ src/main/java/exersite/workoutapi/WorkoutApiApplication.java
@@ -0,0 +1,13 @@
+package exersite.workoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exersite/workoutapi/WorkoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package exersite.workoutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
